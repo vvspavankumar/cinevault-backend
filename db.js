e722cd69e65dcdabf57b356a3a81506de691be81
vvspavankumar <pavanvedasistla@gmail.com>
2026-03-01 18:08:27 +0530

    f

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false } // Required for Railway
 });
 
 db.connect((err) => {
