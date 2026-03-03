2f37fac7eaa80ff8e79b504ed7b0009a043be5ab
vvspavankumar <pavanvedasistla@gmail.com>
2026-03-03 23:07:20 +0530

    fix db for production

--- db.js
+++ db.js
@@ -1,25 +1,26 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-        ssl: { rejectUnauthorized: false }
+let db;
 
-});
+if (process.env.DATABASE_URL) {
+    // Production (Render)
+    db = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+    // Local
+    db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+    });
+}
 
-// Test connection once
-db.getConnection((err, connection) => {
+db.connect((err) => {
     if (err) {
         console.error("❌ DB connection failed:", err);
     } else {
         console.log("✅ Connected to Railway MySQL");
-        connection.release();
     }
 });
 
