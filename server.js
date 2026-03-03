18155fd6188f084957edcddc8019ef19f341071c
vvspavankumar <pavanvedasistla@gmail.com>
2026-03-03 11:57:59 +0530

    final1

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
