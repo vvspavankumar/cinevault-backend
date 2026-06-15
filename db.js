55feec685b4dc56f2dc9d078a600f777782151c4
vvspavankumar <pavanvedasistla@gmail.com>
2026-06-15 20:21:15 +0530

    completed successfull

--- db.js
+++ db.js
@@ -1,18 +1,26 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    uri: process.env.DATABASE_URL,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+let db;
 
-db.getConnection((err, connection) => {
+if (process.env.DATABASE_URL) {
+    // Production (Render)
+    db = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+    // Local
+    db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+    });
+}
+
+db.connect((err) => {
     if (err) {
         console.error("❌ DB connection failed:", err);
     } else {
         console.log("✅ Connected to Railway MySQL");
-        connection.release();
     }
 });
 
